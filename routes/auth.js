9b3b59e6cfcbf8098a779909d226e429fb3a4037
Jason Beh <behjieshen@gmail.com>
2022-04-22 14:06:53 +0930

    Revert to using username instead of email

--- routes/auth.js
+++ routes/auth.js
@@ -18,36 +18,48 @@ var router = express.Router();
 
 
 // Verify Password
-passport.use(new LocalStrategy(function verify(email, password, cb) {
+passport.use(new LocalStrategy(function verify(username, password, cb) {
     // Connect to the database
-    console.log(email);
-    console.log(password);
+    console.log('test');
     db.connectionPool.getConnection(function (err, connection) {
         if (err) { return cb(err); }
         // Query the database
-        var query = "SELECT * FROM Authentication WHERE email = ?";
-        connection.query(query, [email], function (err, rows, fields) {
+        var query = "SELECT * FROM Authentication WHERE username = ?";
+        connection.query(query, [username], function (err, rows, fields) {
             connection.release();
             if (err) { return cb(err); }
-            if (!rows || rows.length == 0) { return cb(null, false, { message: 'Incorrect email or password.' }); }
+            if (!rows || rows.length == 0) { return cb(null, false, { message: 'Incorrect username or password.' }); }
             // Verify the hashed password
             var user = rows[0];
-            console.log(user);
-            // Table types: email,varchar(255) | password,varchar(255)
-            argon2.verify(user["password"], password).then(function() {
+            // Table types: username---varchar(255) | hashed_password,varbinary(255) | salt,varbinary(255)
+            argon2.verify(user["hashed_password"], password).then(function() {
+                console.log('login successfully');
                 return cb(null, rows);
-            }).catch(function() {
-                return cb(null, false, { message: 'Incorrect email or password.' });
+            }).catch(function(err) {
+                console.log('login failure');
+                console.log(err);
+                return cb(null, false, { message: 'Incorrect username or password.' });
             });
+
+
+            // crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+            //     if (err) { return cb(err); }
+            //     if (!crypto.timingSafeEqual(user['hashed_password'], hashedPassword)) {
+            //         return cb(null, false, { message: 'Incorrect username or password.' });
+            //     }
+            //     return cb(null, rows);
+            // });
         });
     });
 }));
 
+
 passport.serializeUser(function (user, cb) {
     process.nextTick(function () {
-        cb(null, { id: user.id, email: user.email });
+        cb(null, { id: user.id, username: user.username });
     });
 });
+
 
 passport.deserializeUser(function (user, cb) {
     process.nextTick(function () {
@@ -55,9 +67,11 @@ passport.deserializeUser(function (user, cb) {
     });
 });
 
+
 router.get('/login', function (req, res, next) {
     res.render('login');
 });
+
 
 // process the form on the login page
 router.post('/login/password', passport.authenticate('local', {
@@ -65,33 +79,42 @@ router.post('/login/password', passport.authenticate('local', {
     failureRedirect: '/login'
 }));
 
+
 router.get('/signup', function (req, res, next) {
     res.render('signup');
 });
 
 router.post('/signup', function (req, res, next) {
     // hashing the password    
-    argon2.hash(req.body.password).then(function(password) {
+    argon2.hash(req.body.password).then(function(hashedPassword) {
         db.connectionPool.getConnection(function (err, connection) {
             if (err) { return next(err); }
             // Query the database
-            var query = "INSERT INTO Authentication (email, password) VALUES (?, ?)";
-            connection.query(query, [req.body.email, password], function (err) {
+            var query = "INSERT INTO Authentication (username, hashed_password) VALUES (?, ?)";
+            connection.query(query, [req.body.username, hashedPassword], function (err) {
                 connection.release();
                 if (err) { return next(err); }
                 var user = {
                     id: this.lastID,
-                    email: req.body.email
+                    username: req.body.username
                 };
                 req.login(user, function (err) {
                     if (err) { return next(err); }
+                    console.log('signup successfully');
                     res.redirect('/');
                 });
             });
         });
     }).catch(function(err) {
+        console.log(err);
         return next(err);
     });
+
+    // crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+    //     if (err) { return next(err); }
+    //     // Connect to the database
+
+    // });
 });
 
 
