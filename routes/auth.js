061b29ab7a497da2b2b1dce453abbc19425554ce
Jason Beh <behjieshen@gmail.com>
2022-04-22 14:38:20 +0930

    Fix passport not allowing email as a field

--- routes/auth.js
+++ routes/auth.js
@@ -16,39 +16,28 @@ var argon2 = require('argon2');
 
 var router = express.Router();
 
-
 // Verify Password
-passport.use(new LocalStrategy(function verify(username, password, cb) {
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, function verify(email, password, cb) {
     // Connect to the database
-    console.log('test');
     db.connectionPool.getConnection(function (err, connection) {
         if (err) { return cb(err); }
         // Query the database
-        var query = "SELECT * FROM Authentication WHERE username = ?";
-        connection.query(query, [username], function (err, rows, fields) {
+        var query = "SELECT * FROM Authentication WHERE email = ?";
+        connection.query(query, [email], function (err, rows, fields) {
             connection.release();
             if (err) { return cb(err); }
-            if (!rows || rows.length == 0) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+            if (!rows || rows.length == 0) { return cb(null, false, { message: 'Incorrect email or password.' }); }
             // Verify the hashed password
             var user = rows[0];
-            // Table types: username---varchar(255) | hashed_password,varbinary(255) | salt,varbinary(255)
-            argon2.verify(user["hashed_password"], password).then(function() {
-                console.log('login successfully');
+            // Table types: email---varchar(255) | password,varbinary(255) | salt,varbinary(255)
+            argon2.verify(user["password"], password).then(function() {
                 return cb(null, rows);
             }).catch(function(err) {
-                console.log('login failure');
-                console.log(err);
-                return cb(null, false, { message: 'Incorrect username or password.' });
+                return cb(null, false, { message: 'Incorrect email or password.' });
             });
-
-
-            // crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
-            //     if (err) { return cb(err); }
-            //     if (!crypto.timingSafeEqual(user['hashed_password'], hashedPassword)) {
-            //         return cb(null, false, { message: 'Incorrect username or password.' });
-            //     }
-            //     return cb(null, rows);
-            // });
         });
     });
 }));
@@ -56,7 +45,7 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
 
 passport.serializeUser(function (user, cb) {
     process.nextTick(function () {
-        cb(null, { id: user.id, username: user.username });
+        cb(null, { id: user.id, email: user.email });
     });
 });
 
@@ -90,31 +79,23 @@ router.post('/signup', function (req, res, next) {
         db.connectionPool.getConnection(function (err, connection) {
             if (err) { return next(err); }
             // Query the database
-            var query = "INSERT INTO Authentication (username, hashed_password) VALUES (?, ?)";
-            connection.query(query, [req.body.username, hashedPassword], function (err) {
+            var query = "INSERT INTO Authentication (email, password) VALUES (?, ?)";
+            connection.query(query, [req.body.email, hashedPassword], function (err) {
                 connection.release();
                 if (err) { return next(err); }
                 var user = {
                     id: this.lastID,
-                    username: req.body.username
+                    email: req.body.email
                 };
                 req.login(user, function (err) {
                     if (err) { return next(err); }
-                    console.log('signup successfully');
                     res.redirect('/');
                 });
             });
         });
     }).catch(function(err) {
-        console.log(err);
         return next(err);
     });
-
-    // crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function (err, hashedPassword) {
-    //     if (err) { return next(err); }
-    //     // Connect to the database
-
-    // });
 });
 
 
