9fa8556906663894b1808b7462229b8b91ca679d
Jason Beh <behjieshen@gmail.com>
2022-04-25 16:45:45 +0930

    Remove specify availability that is commented

--- routes/availability.js
+++ routes/availability.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var db = require('../utils/db');
 const { userIsLoggedIn } = require('../utils/auth');
-const { pathToHtml } = require('../utils/routes');
 
 var router = express.Router();
 
@@ -22,21 +21,5 @@ router.post('/get-all-availability', function (req, res, next) {
     });
 });
 
-// router.post('/specify-availability', function (req, res, next) {
-//     // Ensure an user is logged in
-//     if(!userIsLoggedIn(req.user)) {
-//         return res.status(401).send('Unauthorized Access!!');
-//     }
-//     db.connectionPool.getConnection(function(err, connection) {
-//         if (err) {return next(err); }
-//         var { event_id, start_date, end_date } = req.body;
-//         var query = "INSERT INTO Availability VALUES (?, ?, ?, ?);";
-//         connection.query(query, [req.user.email, event_id, start_date, end_date], function(err, rows, fields) {
-//             connection.release();
-//             if (err) { return next(err); }
-//             return res.send("Success in specifying availability");
-//         });
-//     });
-// });
 
 module.exports = router;
