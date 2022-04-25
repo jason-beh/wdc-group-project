f2b16e05baa8cd01a5ee4dc20126053218933ada
Jason Beh <behjieshen@gmail.com>
2022-04-25 16:44:17 +0930

    Update SQL Schema based on diff availabiliy requirements

--- routes/availability.js
+++ routes/availability.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var db = require('../utils/db');
 const { userIsLoggedIn } = require('../utils/auth');
+const { pathToHtml } = require('../utils/routes');
 
 var router = express.Router();
 
@@ -20,5 +21,22 @@ router.post('/get-all-availability', function (req, res, next) {
         });
     });
 });
+
+// router.post('/specify-availability', function (req, res, next) {
+//     // Ensure an user is logged in
+//     if(!userIsLoggedIn(req.user)) {
+//         return res.status(401).send('Unauthorized Access!!');
+//     }
+//     db.connectionPool.getConnection(function(err, connection) {
+//         if (err) {return next(err); }
+//         var { event_id, start_date, end_date } = req.body;
+//         var query = "INSERT INTO Availability VALUES (?, ?, ?, ?);";
+//         connection.query(query, [req.user.email, event_id, start_date, end_date], function(err, rows, fields) {
+//             connection.release();
+//             if (err) { return next(err); }
+//             return res.send("Success in specifying availability");
+//         });
+//     });
+// });
 
 module.exports = router;
---
+++ sql/deleteAll.sql
@@ -0,0 +1,8 @@
+DROP TABLE Proposed_Event_Time;
+DROP TABLE Availability;
+DROP TABLE Attendance;
+DROP TABLE Availability;
+DROP TABLE Events;
+DROP TABLE User_Profile;
+DROP TABLE Authentication;
+DROP TABLE sessions;
