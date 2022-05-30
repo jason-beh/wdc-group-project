bc2eb8e7bb2ac46f469452514dcf054efca90551
Jason Beh <behjieshen@gmail.com>
2022-05-30 20:55:37 +0930

    search in navbar

--- routes/search.js
+++ routes/search.js
@@ -1,20 +1,39 @@
-var express = require('express');
-var db = require('../utils/db');
+var express = require("express");
+var db = require("../utils/db");
+const { pathToHtml } = require("../utils/routes");
 
 var router = express.Router();
 
-router.get('/search', function(req, res, next) {
-    db.connectionPool.getConnection(function(err, connection) {
-        if (err) { return next(err); }
-        var {search_content} = req.body;
-        var query = `SELECT title, description, proposal_date, start_date, end_date, custom_link, address_line, state, country, postcode from Events WHERE title LIKE '%${search_content}%'`;
-        connection.query(query, function(err, rows, fields) {
-            connection.release();
-            if (err) {return next(err); }
-            return res.send(rows);
-        });
+router.get("/search", function (req, res, next) {
+  let { q } = req.query;
+
+  if (!q) {
+    return res.send([]);
+  }
+
+  db.connectionPool.getConnection(function (err, connection) {
+    if (err) {
+      return next(err);
+    }
+
+    var query = `SELECT * from Events WHERE title LIKE '%${q}%'`;
+    connection.query(query, function (err, rows, fields) {
+      connection.release();
+      if (err) {
+        return next(err);
+      }
+      return res.send(rows);
     });
+  });
 });
 
+router.post("/search", function (req, res, next) {
+  return res.redirect(`/search-results?q=${req.body.query}`);
+});
+
+// Rendering Pages
+router.get("/search-results", function (req, res, next) {
+  res.sendFile(pathToHtml("search-results.html"));
+});
 
 module.exports = router;
