bb189cc64efe803800d1f102a7b06f51c1ceedfe
Jiajun-github-io <1372207033@qq.com>
2022-06-07 18:09:52 +0930

    Handled some edge cases

--- routes/attendance.js
+++ routes/attendance.js
@@ -28,78 +28,73 @@ router.get(
         if (err) {
           return res.status(500).send("An interval server error occurred.");
         }
+
         if (!rows || rows.length === 0) {
-          return res
-            .status(405)
-            .send("The event does not exist.");
-        } else if (!rows[0]["finalized_event_time_id"]) {
+          return res.status(405).send("The event does not exist.");
+        }
+        if (!rows[0]["finalized_event_time_id"]) {
           return res
             .status(405)
             .send("You cannot confirm attendance before the event is finalized.");
-        } else {
-          query = "SELECT * from Attendance where email = ? and event_id = ?;";
+        }
+        query = "SELECT * from Attendance where email = ? and event_id = ?;";
+        connection.query(query, [email, event_id], function (err, rows, fields) {
+          if (err) {
+            console.log(err);
+            return res.status(500).send("An interval server error occurred.");
+          }
+
+          if (rows && rows.length > 0) {
+            return res.status(500).send("Availability has been previously specified");
+          }
+
+          query = "insert into Attendance values (?, ?);";
           connection.query(query, [email, event_id], function (err, rows, fields) {
+            connection.release();
             if (err) {
               console.log(err);
               return res.status(500).send("An interval server error occurred.");
             }
 
-            if (rows && rows.length > 0) {
-              return res.status(500).send("Availability has been previously specified");
-            }
-
-            query = "insert into Attendance values (?, ?);";
-            connection.query(query, [email, event_id], function (err, rows, fields) {
-              connection.release();
+            // Send email to the creator
+            query = "SELECT * FROM Events WHERE event_id = ?";
+            connection.query(query, [event_id], function (err, rows, fields) {
               if (err) {
-                console.log(err);
                 return res.status(500).send("An interval server error occurred.");
               }
 
-              // Send email to the creator
-              query = "SELECT * FROM Events WHERE event_id = ?";
-              connection.query(query, [event_id], function (err, rows, fields) {
+              let event = rows[0];
+
+              query = "SELECT * FROM Notifications_Setting WHERE email = ?";
+              connection.query(query, [event["created_by"]], function (err, settingsRows, fields) {
                 if (err) {
                   return res.status(500).send("An interval server error occurred.");
                 }
 
-                let event = rows[0];
-
-                query = "SELECT * FROM Notifications_Setting WHERE email = ?";
-                connection.query(
-                  query,
-                  [event["created_by"]],
-                  function (err, settingsRows, fields) {
-                    if (err) {
-                      return res.status(500).send("An interval server error occurred.");
+                // Send an email if they enable notifications
+                if (settingsRows[0]["is_availability_confirmed"] === 1) {
+                  var mailOptions = {
+                    from: "socialah@outlook.com", // sender address (who sends)
+                    to: settingsRows[0]["email"],
+                    subject: "Specified Availability", // Subject line
+                    text: "Hello world ", // plaintext body
+                  };
+                  mailOptions[
+                    "html"
+                  ] = `<h1>${email} has specified their availability for ${event["title"]}</h1>`;
+                  // send mail with defined transport object
+                  req.transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                      return console.log(error);
                     }
-
-                    // Send an email if they enable notifications
-                    if (settingsRows[0]["is_availability_confirmed"] === 1) {
-                      var mailOptions = {
-                        from: "socialah@outlook.com", // sender address (who sends)
-                        to: settingsRows[0]["email"],
-                        subject: "Specified Availability", // Subject line
-                        text: "Hello world ", // plaintext body
-                      };
-                      mailOptions[
-                        "html"
-                      ] = `<h1>${email} has specified their availability for ${event["title"]}</h1>`;
-                      // send mail with defined transport object
-                      req.transporter.sendMail(mailOptions, function (error, info) {
-                        if (error) {
-                          return console.log(error);
-                        }
-                      });
-                    }
-                  }
-                );
-
-                return res.status(200).end();
+                  });
+                }
               });
+
+              return res.status(200).end();
             });
           });
-        }
+        });
       });
     });
   }
--- routes/availability.js
+++ routes/availability.js
@@ -50,53 +50,54 @@ router.post("/specify-availability", function (req, res, next) {
       }
       if (!rows || rows.length === 0) {
         return res.status(405).send("The event does not exist.");
-      } else if (rows[0]["finalized_event_time_id"]) {
+      }
+      if (rows[0]["finalized_event_time_id"]) {
         return res
           .status(405)
           .send("You cannot specify availability after the event is finalized.");
-      } else {
-        let baseQueryOptions;
+      }
 
-        if (!userIsLoggedIn(req.session.user)) {
-          if (!req.body.email) {
-            return res.status(400).send("Insufficient Data");
-          }
-          check(req.body.email).isEmail().withMessage("Email is invalid");
-          const errors = validationResult(req);
-          if (!errors.isEmpty()) {
-            let error = errors.array()[0];
-            return res.status(400).send(error.msg);
-          }
-          baseQueryOptions = [req.body["email"]];
-        } else {
-          baseQueryOptions = [req.session.user.email];
+      let baseQueryOptions;
+
+      if (!userIsLoggedIn(req.session.user)) {
+        if (!req.body.email) {
+          return res.status(400).send("Insufficient Data");
+        }
+        check(req.body.email).isEmail().withMessage("Email is invalid");
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          let error = errors.array()[0];
+          return res.status(400).send(error.msg);
+        }
+        baseQueryOptions = [req.body["email"]];
+      } else {
+        baseQueryOptions = [req.session.user.email];
+      }
+
+      // delete previous availability
+      let queryOptions = [...baseQueryOptions, event_id];
+      let query =
+        "delete Availability from Availability inner join Proposed_Event_Time on Availability.proposed_event_time_id = Proposed_Event_Time.proposed_event_time_id where email = ? and event_id = ?;";
+      connection.query(query, queryOptions, function (err, rows, fields) {
+        if (err) {
+          return res.status(500).send("An interval server error occurred.");
         }
 
-        // delete previous availability
-        let queryOptions = [...baseQueryOptions, event_id];
-        let query =
-          "delete Availability from Availability inner join Proposed_Event_Time on Availability.proposed_event_time_id = Proposed_Event_Time.proposed_event_time_id where email = ? and event_id = ?;";
-        connection.query(query, queryOptions, function (err, rows, fields) {
-          if (err) {
-            return res.status(500).send("An interval server error occurred.");
-          }
-
-          // insert new availability
-          for (let index = 0; index < proposed_event_id.length; index++) {
-            let queryOptions = [...baseQueryOptions, proposed_event_id[index]];
-            console.log(queryOptions);
-            let query = "INSERT INTO Availability VALUES (?, ?);";
-            connection.query(query, queryOptions, function (err, rows, fields) {
-              if (err) {
-                console.log(err);
-                return res.status(500).send("An interval server error occurred.");
-              }
-            });
-          }
-          connection.release();
-          return res.send("Successfully in editing availability");
-        });
-      }
+        // insert new availability
+        for (let index = 0; index < proposed_event_id.length; index++) {
+          let queryOptions = [...baseQueryOptions, proposed_event_id[index]];
+          console.log(queryOptions);
+          let query = "INSERT INTO Availability VALUES (?, ?);";
+          connection.query(query, queryOptions, function (err, rows, fields) {
+            if (err) {
+              console.log(err);
+              return res.status(500).send("An interval server error occurred.");
+            }
+          });
+        }
+        connection.release();
+        return res.send("Successfully in editing availability");
+      });
     });
   });
 });
