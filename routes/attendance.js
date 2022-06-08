999fb73e05980dca88971c01b6015d68a4af9343
Jason Beh <behjieshen@gmail.com>
2022-06-08 16:50:11 +0930

    Fix myevents and move email logic from confirm attendance to specify availability

--- routes/attendance.js
+++ routes/attendance.js
@@ -56,43 +56,7 @@ router.get(
               return res.status(500).send("An interval server error occurred.");
             }
 
-            // Send email to the creator
-            query = "SELECT * FROM Events WHERE event_id = ?";
-            connection.query(query, [event_id], function (err, rows, fields) {
-              if (err) {
-                return res.status(500).send("An interval server error occurred.");
-              }
-
-              let event = rows[0];
-
-              query = "SELECT * FROM Notifications_Setting WHERE email = ?";
-              connection.query(query, [event["created_by"]], function (err, settingsRows, fields) {
-                if (err) {
-                  return res.status(500).send("An interval server error occurred.");
-                }
-
-                // Send an email if they enable notifications
-                if (settingsRows[0]["is_availability_confirmed"] === 1) {
-                  var mailOptions = {
-                    from: "socialah@outlook.com", // sender address (who sends)
-                    to: settingsRows[0]["email"],
-                    subject: "Specified Availability", // Subject line
-                    text: "Hello world ", // plaintext body
-                  };
-                  mailOptions[
-                    "html"
-                  ] = `<h1>${email} has specified their availability for ${event["title"]}</h1>`;
-                  // send mail with defined transport object
-                  req.transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                      return console.log(error);
-                    }
-                  });
-                }
-              });
-
-              return res.status(200).end();
-            });
+            return res.status(200).end();
           });
         });
       });
